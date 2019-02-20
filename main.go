4bd92cbbe070e77d671a1347509b7e911b0e2310
ddiffa <ddiffa2@gmail.com>
2019-02-21 01:17:23 +0700

    add POST Method

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 type Book struct {
@@ -45,7 +47,13 @@ func getBook(w http.ResponseWriter, r *http.Request){
 }
 
 func createBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000))
+	books = append(books, book)
 
+	json.NewEncoder(w).Encode(book)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request){
