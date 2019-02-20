739146ef11e897dc6e5d85fa45031ff3165d1623
ddiffa <ddiffa2@gmail.com>
2019-02-21 01:20:37 +0700

    add DELETE Method

--- main.go
+++ main.go
@@ -61,7 +61,15 @@ func updateBook(w http.ResponseWriter, r *http.Request){
 }
 
 func deleteBooks(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	for index, item := range books   {
+		if item.ID == params["id"] {
+			books = append(books[:index],books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 
